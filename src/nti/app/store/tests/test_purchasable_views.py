e4b3d3b61370e7353cd3203c0cd72be44d05f39b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-07 20:40:58 +0000

    admin view to create a purchasable
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.store/trunk@64709 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/store/tests/test_purchasable_views.py
+++ src/nti/app/store/tests/test_purchasable_views.py
@@ -7,19 +7,73 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import none
 from hamcrest import is_not
+from hamcrest import assert_that
+from hamcrest import has_entries
 does_not = is_not
+
+import fudge
+
+from nti.store.purchasable import get_purchasable
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.app.store.tests import ApplicationStoreTestLayer
 
+import nti.dataserver.tests.mock_dataserver as mock_dataserver
+
 class TestStoreViews(ApplicationLayerTest):
 
 	layer = ApplicationStoreTestLayer
+	
+	purchasalbe = {
+		'Amount': 300.0,
+		'Author': u'CMU',
+		'BulkPurchase': True,
+		'Class': 'Purchasable',
+		'Currency': u'USD',
+		'Description': u'04-630: Computer Science for Practicing Engineers',
+		'Discountable': True,
+		'Fee': None,
+		'Giftable': False,
+		'Icon': u'http://cmu.edu/',
+		'IsPurchasable': True,
+		'Items': [u'tag:nextthought.com,2011-10:CMU-HTML-04630_main.04_630:_computer_science_for_practicing_engineers'],
+		'License': u'1 Year License',
+		'MimeType': 'application/vnd.nextthought.store.purchasable',
+		'NTIID': u'tag:nextthought.com,2011-10:CMU-purchasable-computer_science_for_practicing_engineer',
+		'Provider': u'CMU',
+		'Public': True,
+		'Redeemable': False}
+	
+	@WithSharedApplicationMockDS(users=True, testapp=True)
+	def test_get_legacy_purchasable(self):
+		ntiid = "tag:nextthought.com,2011-10:CMU-HTML-04630_main.04_630:_computer_science_for_practicing_engineers"
+		url = '/dataserver2/Purchasables/%s' % ntiid
+		self.testapp.get(url, status=200)
 
 	@WithSharedApplicationMockDS(users=True, testapp=True)
-	def test_purchasables(self):
-		url = '/dataserver2/Purchasables/xx'
-		self.testapp.get(url, status=404)
+	@fudge.patch('nti.app.store.views.purchasable_views.validate_purchasble')
+	def test_create_purchasable(self, mock_vp):
+		mock_vp.is_callable().with_args().returns(None)
+				
+		ntiid = u'tag:nextthought.com,2011-10:CMU-purchasable-computer_science_for_practicing_engineer'
+
+		with mock_dataserver.mock_db_trans(self.ds):
+			p = get_purchasable(ntiid)
+			assert_that(p, is_(none()))
+		
+		url = '/dataserver2/store/create_purchasable'
+		res = self.testapp.post_json(url, self.purchasalbe, status=201)
+		assert_that(res.json_body, 
+					has_entries('OID', is_not(none()),
+								'Amount', is_(300.0),
+								'NTIID', ntiid ) )
+
+		with mock_dataserver.mock_db_trans(self.ds):
+			p = get_purchasable(ntiid)
+			assert_that(p, is_not(none()))
+
