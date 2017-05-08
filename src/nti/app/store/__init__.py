b553a3e1a133824a45d55a6c5561091d98c5ec34
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-08 03:32:03 +0000

    kill unicode_literals
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.store/trunk@112222 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/store/__init__.py
+++ src/nti/app/store/__init__.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -16,10 +16,10 @@ from nti.site.runner import get_possible_site_names
 
 
 #: Store path
-STORE = 'store'
+STORE = u'store'
 
 #: Stripe path
-STRIPE = 'stripe'
+STRIPE = u'stripe'
 
 #: Purchasables path
-PURCHASABLES = 'purchasables'
+PURCHASABLES = u'purchasables'
