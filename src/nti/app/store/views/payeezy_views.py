9178a2cb0248e99e7939222ea675c011904d756a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-10 18:04:14 +0000

    add Payeezy pricing views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.store/trunk@112449 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/store/views/payeezy_views.py
+++ src/nti/app/store/views/payeezy_views.py
@@ -24,15 +24,24 @@ from nti.app.store import MessageFactory as _
 
 from nti.app.store.views import PayeezyPathAdapter
 
+from nti.app.store.views.general_views import PricePurchasableView as GeneralPricePurchasableView
+
+from nti.app.store.views.view_mixin import PriceOrderViewMixin
 from nti.app.store.views.view_mixin import BaseProcessorViewMixin
 from nti.app.store.views.view_mixin import RefundPaymentViewMixin
 from nti.app.store.views.view_mixin import GetProcesorConnectKeyViewMixin
+
+from nti.app.store.views.view_mixin import price_order
 
 from nti.dataserver import authorization as nauth
 
 from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
 
+from nti.store import PricingException
+from nti.store import InvalidPurchasable
+
+from nti.store.interfaces import IPricingError
 from nti.store.interfaces import IPaymentProcessor
 
 from nti.store.payments.payeezy import PAYEEZY
@@ -45,7 +54,10 @@ LAST_MODIFIED = StandardExternalFields.LAST_MODIFIED
 
 class BasePayeezyView(BaseProcessorViewMixin):
     processor = PAYEEZY
-    key_interface = IPayeezyConnectKey   
+    key_interface = IPayeezyConnectKey
+
+
+# keys
 
 
 @view_config(name="GetConnectKey")
@@ -55,7 +67,48 @@ class BasePayeezyView(BaseProcessorViewMixin):
                permission=nauth.ACT_READ,
                context=PayeezyPathAdapter,
                request_method='GET')
-class GetConnectKeyView(GetProcesorConnectKeyViewMixin, BasePayeezyView):
+class GetConnectKeyView(GetProcesorConnectKeyViewMixin,
+                        BasePayeezyView):
+    pass
+
+
+# pricing
+
+
+@view_config(name="PriceOrder")
+@view_config(name="price_order")
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               context=PayeezyPathAdapter,
+               request_method='POST')
+class PriceOrderView(PriceOrderViewMixin, BasePayeezyView):
+
+    def _do_pricing(self, order):
+        try:
+            result = price_order(order, self.processor)
+        except InvalidPurchasable:
+            result = IPricingError(_(u"Invalid purchasable."))
+        except PricingException as e:
+            result = IPricingError(e)
+        except Exception:
+            raise
+        return result
+
+    def _do_call(self):
+        order = self.readCreateUpdateContentObject()
+        result = self._do_pricing(order)
+        status = 422 if IPricingError.providedBy(result) else 200
+        self.request.response.status_int = status
+        return result
+
+
+@view_config(name="PricePurchasable")
+@view_config(name="price_purchasable")
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               context=PayeezyPathAdapter,
+               request_method='POST')
+class PricePurchasableView(GeneralPricePurchasableView):
     pass
 
 
@@ -64,7 +117,7 @@ class GetConnectKeyView(GetProcesorConnectKeyViewMixin, BasePayeezyView):
 
 def refund_purchase(purchase, amount, request=None):
     manager = component.getUtility(IPaymentProcessor, name=PAYEEZY)
-    return manager.refund_purchase(purchase, 
+    return manager.refund_purchase(purchase,
                                    amount=amount,
                                    request=request)
 
@@ -82,7 +135,7 @@ class RefundPaymentView(RefundPaymentViewMixin, BasePayeezyView):
         request = self.request
         purchase, amount = self.processInput()
         try:
-            refund_purchase(purchase, 
+            refund_purchase(purchase,
                             amount=amount,
                             request=request)
         except Exception as e:
@@ -90,14 +143,14 @@ class RefundPaymentView(RefundPaymentViewMixin, BasePayeezyView):
             exc_info = sys.exc_info()
             raise_error(request,
                         hexc.HTTPUnprocessableEntity,
-                        {    
+                        {
                             'message': _(u"Error while refunding transaction."),
                             'code': e.__class__.__name__
                         },
                         exc_info[2])
 
         result = LocatedExternalDict({
-                    ITEMS: [purchase],
-                    LAST_MODIFIED: purchase.lastModified
-                 })
+            ITEMS: [purchase],
+            LAST_MODIFIED: purchase.lastModified
+        })
         return result
