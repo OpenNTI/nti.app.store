63a6467e67a4279b12ccbd25444423b423e7a2d0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-31 00:05:24 +0000

    add supporting file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.store/trunk@113807 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- README.rst
+++ README.rst
@@ -1,1 +1,9 @@
+===============
+ nti.app.store
+===============
 
+.. image:: https://travis-ci.org/NextThought/nti.app.store.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.store
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.store/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.store?branch=master
--- setup.py
+++ setup.py
@@ -1,43 +1,46 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
-
 entry_points = {
-	'console_scripts': [
-	],
-	"z3c.autoinclude.plugin": [
-		'target = nti.app',
-	],
+    'console_scripts': [
+    ],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
 }
 
-import platform
-py_impl = getattr(platform, 'python_implementation', lambda: None)
-IS_PYPY = py_impl() == 'PyPy'
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
-	name='nti.app.store',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI Store Integration",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid preference',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: Implementation :: CPython'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app'],
-	install_requires=[
-		'setuptools',
-		'nti.store'
-	],
-	entry_points=entry_points
+    name='nti.app.store',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Store Integration",
+    long_description=(_read('README.rst') + '\n\n' + _read('CHANGES.rst')),
+    license='Apache',
+    keywords='pyramid store',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app'],
+    install_requires=[
+        'setuptools',
+        'nti.store',
+    ],
+    entry_points=entry_points,
+    test_suite="nti.app.store.tests",
 )
