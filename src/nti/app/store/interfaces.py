846ab8fcf740d38bcefe99159cfd340388e4c684
Josh Zuech <josh.zuech@nextthought.com>
2020-04-17 15:37:45 -0500

    Purchasable default field provider

--- src/nti/app/store/interfaces.py
+++ src/nti/app/store/interfaces.py
@@ -8,6 +8,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from zope import interface
+
 from nti.app.products.integration.interfaces import IIntegration
 
 from nti.appserver.workspaces.interfaces import IWorkspace
@@ -17,6 +19,22 @@ class IStoreWorkspace(IWorkspace):
     """
     A workspace containing data for store.
     """
+
+
+class IPurchasableDefaultFieldProvider(interface.Interface):
+    """
+    A utility that will provide some basic default fields for use
+    when a new purchasable is created via the API.
+    """
+
+    def get_default_fee():
+        pass
+
+    def get_default_provider():
+        pass
+
+    def get_default_currency():
+        pass
 
 
 class IStripeIntegration(IIntegration):
